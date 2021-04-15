825312f20121e738a7152ffc185abc8986510af1
Olof Beckman <olof.beckman@gmail.com>
2021-04-15 12:19:53 +0000

    Parsing some basic indicators

--- scripts/stix.py
+++ scripts/stix.py
@@ -3,7 +3,23 @@ from rich import print
 from rich.console import Console
 from rich.table import Table
 from rich.tree import Tree
-import json
+import orjson
+from pydantic import BaseModel
+from typing import Optional, List, Dict
+from datetime import datetime
+
+
+class Malware(BaseModel):
+    id: str
+    name: Optional[str]
+    created: datetime
+
+
+class Indicator(BaseModel):
+    id: str
+    patterns: Dict[str,str]
+    indicates: Optional[Malware]
+
 
 @click.command()
 @click.argument('filenames', nargs=-1, type=click.Path())
@@ -12,18 +28,44 @@ def read_json(filenames):
     console=Console()
     
     for filename in filenames:
+        malwares = dict()
+        indicators = dict()
+        relations = list()
+
         table = Table(title=filename)
         tree = Tree(filename)
 
         with open(filename) as fd:
-            stixes = json.load(fd)['objects']
+            stixes = orjson.loads(fd.read())['objects']
         
-        for stix in stixes[:100]:
-            stix_tree = tree.add(stix.get('name', 'unnamed'))
-            for key in stix:
-                stix_tree.add(f"{key}: {stix[key]}")
+        for stix in stixes:
+            if stix['type'] == 'malware':
+                malware = Malware(**stix)
+                malwares[malware.id] = malware
 
-        console.print(tree)
+            if stix['type'] == 'indicator':
+                patterns = dict()
+                stix['pattern'] = stix.get('pattern','').strip('[]')
+                for pattern in stix['pattern'].split(' '):
+                    if '=' in pattern:
+                        key, value = pattern.split('=')
+                        patterns[key] = value
+                stix['patterns'] = patterns
+
+                indicator = Indicator(**stix)
+                indicators[indicator.id] = indicator
+            
+            if stix['type'] == 'relationship':
+                source_type, source = stix['source_ref'].split('--')
+                target_type, target = stix['target_ref'].split('--')
+                relations.append((source, target))
+
+        for source, target in relations:
+            if source in indicators and target in malwares:
+                indicators[source].indicates = malwares.target
+                print(indicators[source])
+            
+        # console.print(indicators)
 
     
 
