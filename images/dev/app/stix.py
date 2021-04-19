9666591c6da902ab2f305faebf796d3e95940921
Olof Beckman <olof.beckman@gmail.com>
2021-04-19 15:15:33 +0000

    Create index template automatically

--- images/dev/app/stix.py
+++ images/dev/app/stix.py
@@ -4,6 +4,7 @@ from rich.console import Console
 from rich.table import Table
 from rich.tree import Tree
 from rich.traceback import install
+from rich.progress import track
 import orjson
 from pydantic import BaseModel
 from typing import Optional, List, Dict, TextIO
@@ -14,8 +15,9 @@ from glob import glob
 from itertools import chain
 import asyncio
 
-console=Console()
-install(show_locals=True)
+console = Console()
+install(show_locals=False)
+
 
 class Malware(BaseModel):
     id: str
@@ -27,7 +29,7 @@ class Malware(BaseModel):
 
 class Indicator(BaseModel):
     id: str
-    patterns: Dict[str,str]
+    patterns: Dict[str, str]
 
     indicates: Optional[Malware]
 
@@ -40,7 +42,7 @@ class Indicator(BaseModel):
         source['id'] = self.id
         if self.indicates:
             source['indicates'] = self.indicates.id
-        
+
         for key, value in self.patterns.items():
             source[key] = value
 
@@ -61,10 +63,12 @@ pattern_fields = {
 class StixReader():
     """Reads stix from json files and returns ES ready indicators."""
 
-    def __init__(self, index:str = 'indicators'):
+    def __init__(self, index: str = 'indicators'):
         self.index = index
+
         self.malwares = dict()
         self.indicators = dict()
+        self.missing_patterns = set()
 
     async def stix_converter(self, stixes: List[Dict]):
         """Converts stix dicts to ES ready indicators."""
@@ -76,13 +80,15 @@ class StixReader():
 
             if stix['type'] == 'indicator':
                 patterns = dict()
-                stix['pattern'] = stix.get('pattern','').strip('[]')
+                stix['pattern'] = stix.get('pattern', '').strip('[]')
                 for pattern in stix['pattern'].split(' '):
                     if '=' in pattern:
                         key, value = pattern.split('=', 1)
                         if key not in pattern_fields:
-                            console.log(f"No pattern defined for '{key}', skipping.")
+                            # console.log(f"No pattern defined for '{key}', skipping.")
+                            self.missing_patterns.add(key)
                             continue
+
                         key = pattern_fields[key]
                         patterns[key] = value.strip("'")
                 stix['patterns'] = patterns
@@ -101,7 +107,7 @@ class StixReader():
                     indicator.indicates = malware
 
                     yield indicator.to_es(self.index)
-                
+
     async def flush_unmatched(self):
         """Returns the indicators that haven't been matched to any malware (hopefully none)."""
 
@@ -109,65 +115,98 @@ class StixReader():
             yield indicator.to_es(self.index)
 
 
-async def read_json(filenames: List[str], es: AsyncElasticsearch):
+async def read_json(
+        filenames: List[str],
+        es: AsyncElasticsearch,
+        index_name: str = 'indicators',
+    ):
     """Reads stixes from jsons and posts them to Elasticsearch."""
 
     info = await es.info()
-    print(f"Connected to Elasticsearch, version {info['version']['number']}.")
+    console.log(
+        f"Connected to Elasticsearch, version {info['version']['number']}.")
 
-    reader = StixReader('indicators')
-    for filename in filenames:
-        console.log(f"Reading from {filename}.")
+    if await es.indices.exists(index=index_name):
+        console.log(f"Index {index_name} already exists, skipping template load.")
+    else:
+        console.log(f"Uploading index templates...")
+        template = {
+            "index_patterns": [
+                index_name
+            ],
+            "composed_of": [
+                "logs-mappings",
+                "logs-settings"
+            ]
+        }
+        await es.indices.put_index_template(name=index_name, body=template)
+
+    reader = StixReader(index_name)
+    uploads = 0
+    for filename in track(filenames, "Reading files..."):
+        # console.log(f"Reading from {filename}.")
         with open(filename) as fd:
-            stixes = orjson.loads(fd.read())['objects']
-        
-            successful, errors = await async_bulk(es, reader.stix_converter(stixes))
-            console.log(f"{successful} stixes imported successfully.")
+            try:
+                stixes = orjson.loads(fd.read())['objects']
+            except orjson.JSONDecodeError:
+                console.log(f"JSONDecodeError while reading {filename}.")
+                continue
 
-    console.log(f"After importing, {len(reader.indicators)} indicators haven't been matched.")
-    
+            successful, errors = await async_bulk(es, reader.stix_converter(stixes))
+            uploads += successful
+            # console.log(f"{successful} stixes imported successfully.")
+
+    console.log(f"{uploads} stixes uploaded successfully.")
+    if reader.missing_patterns:
+        console.log(f"These patterns were missing: {reader.missing_patterns}.")
+
+    console.log(
+        f"After importing, {len(reader.indicators)} indicators haven't been matched.")
+
     console.log(f"Flushing...")
     successful, errors = await async_bulk(es, reader.flush_unmatched())
     console.log(f"{successful} indicators imported successfully.")
 
     await es.close()
 
-         
+
 @click.command()
 @click.argument('filenames', nargs=-1, type=click.Path())
-@click.option('-t', '--tree_view', is_flag=True)
 @click.option('-e', '--es_host')
 @click.option('-u', '--es_username')
 @click.option('-p', '--es_password')
+@click.option('--index_name', default='indicators')
 def main(
-    filenames: List,
-    tree_view: bool,
-    es_host:str = None,
-    es_username:str = None,
-    es_password:str = None):
+        filenames: List,
+        es_host: str = None,
+        es_username: str = None,
+        es_password: str = None,
+        index_name: str = 'indicators',
+    ):
     """Read stix from files."""
-    
+
     console.log("Stixreader starting...")
 
     filenames = [glob(filename) for filename in filenames]
-    filenames = chain(*filenames)
+    filenames = list(chain(*filenames))
 
     if es_host:
         console.log("Connecting to Elasticsearch...")
         if es_username:
             if not es_password:
                 es_password = click.prompt('Enter password', hide_input=True)
-            es = AsyncElasticsearch(es_host, http_auth=(es_username, es_password))
+            es = AsyncElasticsearch(
+                es_host, http_auth=(es_username, es_password))
         else:
             es = AsyncElasticsearch(es_host)
 
-        asyncio.run(read_json(filenames, es))
+        asyncio.run(read_json(filenames, es, index_name))
 
     else:
         es = None
 
-           # console.print(tree)
+        # console.print(tree)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main(auto_envvar_prefix='STIX')
-
